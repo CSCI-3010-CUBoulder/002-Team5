fbccf7a7fdda6f9c0f9b2106407e74b02e6790ed
aaron <aali7075@colorado.edu>
2020-02-11 10:13:29 -0800

    Added function

---
+++ UtilityFunctions.cpp
@@ -0,0 +1,9 @@
+#include "UtilityFunctions.h"
+
+
+std::vector <int> VectorTimes(std::vector <int> x, int mult){
+  int size=x.size();
+  for(int i=0; i<size; i++){
+    x[i]*=mult;
+  }
+}
---
+++ UtilityFunctions.h
@@ -0,0 +1,9 @@
+#ifndef ULTILITYFUNCTION_H
+#define ULTILITYFUNCTION_H
+
+#include <iostream>
+#include <vector>
+
+std::vector <int> VectorTimes(std::vector <int> x, int mult);
+
+#endif  // ULTILITYFUNCTION_H
