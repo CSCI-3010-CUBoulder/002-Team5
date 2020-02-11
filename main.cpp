b57071d7c601ab75b0a0b52dbbf5487a58cc03d4
JosephineMartin <joma1927@colorado.edu>
2020-02-11 11:36:44 -0700

    Changed main and fix part of the makefile

--- main.cpp
+++ main.cpp
@@ -4,5 +4,18 @@ using namespace std;
 
 int main()
 {
+  std::vector<int> v = {1,2,3};
+  cout <<"Old vector: ";
+  for (int i=0; i<(int)v.size(); i++)
+  {
+    cout << v[i] << " ";
+  }
+  cout << endl << "New vector: ";
+  v = VectorPlusN(5, v);
+  for (int i=0; i<(int)v.size(); i++)
+  {
+    cout << v[i] << " ";
+  }
+  cout << endl;
   return 0;
 }
