54dfdecf7d0b4b04a0dc7519faa5a2af2eda4afa
Josephine Martin <49414457+JosephineMartin@users.noreply.github.com>
2020-02-13 11:23:39 -0700

    Update with vectorPlusN funciton

--- UtilityFunctions.cpp
+++ UtilityFunctions.cpp
@@ -9,3 +9,12 @@ std::vector <int> VectorTimes(std::vector <int> x, int mult){
 
   return x;
 }
+
+std::vector<int> VectorPlusN(int n, std::vector<int> v)
+{
+  for (int i = 0; i<(int)v.size(); i++)
+  {
+    v[i] = v[i]+n;
+  }
+  return v;
+}
